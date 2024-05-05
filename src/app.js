de6a48244a4386d9c88536db58e6a1ffe6af23fc
krishsahu6 <krishsahu601@gmail.com>
2024-05-06 00:05:12 +0530

    setup first router and used postman

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import cookieparser from "cookie-parser"
+import cookieParser from "cookie-parser";
 
 
 const app = express();
@@ -12,6 +12,13 @@ app.use(cors({
 app.use(express.json({limit:"20kb"}))
 app.use(express.urlencoded({extended:true,limit:"20kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
+//routes
+
+import userRouter from "./routes/user.routes.js"
+
+//router declaration
+app.use("/api/v1/users",userRouter)
 
 export {app}
